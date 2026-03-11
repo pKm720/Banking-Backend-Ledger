8a55e249f15ca815b94dfe0027e833770e62da6d
pKm720 <priyanshumahato0210@gmail.com>
2026-03-11 16:34:15 +0530

    Added middleware for transaction from SystemUser

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -25,7 +25,39 @@ async function verifyToken (req,res,next){
             message: "Unauthorization access, Invalid token given"
         })
     }
-} 
+}
+
+async function authSystemMiddleware(req, res, next) {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorization access, Invalid token given"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SEC)
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser")
+        if(!user.systemUser){
+            return res.status(403).json({
+                message: "Forbidden access, not a system user"
+            })  
+        }
+
+        req.user = user
+
+        return next()
+
+    }catch (err){
+        return res.status(401).json({
+            message: "Unauthorization access, Invalid token given"
+        })
+    }
+}
+
+
 
 module.exports = {
     verifyToken
