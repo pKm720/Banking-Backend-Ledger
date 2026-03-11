e2eace5487fbeeb74165178701ba01b08de3f4ef
pKm720 <priyanshumahato0210@gmail.com>
2026-03-11 23:21:28 +0530

    Idempotency check added along with account status check

--- src/controllers/trans.controllers.js
+++ src/controllers/trans.controllers.js
@@ -27,8 +27,34 @@ async function creatTransAction(req,res) {
         })
     }
 
+    const isIdempotencyKeyAvailable = await transactionModel.findOne({
+        idempotencyKey : idempotencyKey
+    })
 
+    if(isIdempotencyKeyAvailable){
+        if(isIdempotencyKeyAvailable.status == "PENDING"){
+            return res.status(200).json({
+                message:"Transaction is being processed"
+            })
+        }
+        if (isTransactionAlreadyExists.status === "COMPLETED") {
+            return res.status(200).json({
+                message: "Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+        if (isTransactionAlreadyExists.status === "FAILED") {
+            return res.status(500).json({
+                message: "Transaction processing failed, please retry"
+            })
+        }
 
+        if (isTransactionAlreadyExists.status === "REVERSED") {
+            return res.status(500).json({
+                message: "Transaction was reversed, please retry"
+            })
+        }
+    }
 }
 
 async function createSystemTransaction(req, res) {
