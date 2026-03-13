b712307d70f41de3d83df6db3d6d43ac56e8cf33
pKm720 <priyanshumahato0210@gmail.com>
2026-03-13 15:24:07 +0530

    Added  Helmet  Morgan  CORS  Mongo sanitize

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const accountRouter = require("./routes/account.route")
 const transactionRouter = require("./routes/transaction.route")
 const rateLimit = require("express-rate-limit")
 const app = express()
-
+const helmet = require("helmet")
+const morgan = require("morgan")
+const mongoSanitize = require("express-mongo-sanitize")
+const cors = require("cors")
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,  // 15 minutes
@@ -21,5 +24,12 @@ app.use("/api/auth",authRouter)
 app.use("/api/account",accountRouter)
 app.use("/api/transaction",transactionRouter)
 app.use(limiter)
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(mongoSanitize())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true
+}))
 
 module.exports = app
