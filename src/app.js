8f7b0478810b6072addb1d8d88d1ccc12b70ae88
pKm720 <priyanshumahato0210@gmail.com>
2026-03-13 13:00:50 +0530

    Rate limiting added

--- src/app.js
+++ src/app.js
@@ -3,15 +3,23 @@ const cookieParser = require("cookie-parser")
 const authRouter = require("./routes/auth.route")
 const accountRouter = require("./routes/account.route")
 const transactionRouter = require("./routes/transaction.route")
-
+const rateLimit = require("express-rate-limit")
 const app = express()
 
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,  // 15 minutes
+    max: process.env.NODE_ENV === "development" ? 1000 : 100, // 100 requests per 15 mins
+    message: {
+        message: "Too many requests, please try again later"
+    }
+})
 
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
 app.use("/api/account",accountRouter)
 app.use("/api/transaction",transactionRouter)
-
+app.use(limiter)
 
 module.exports = app
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const authController = require("../controllers/auth.controllers")
-
+const rateLimit = require("express-rate-limit")
 const router = express.Router()
 
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,  // 15 minutes
+    max: process.env.NODE_ENV === "development" ? 1000 : 5, // 5 requests per 15 mins
+    message: {
+        message: "Too many requests, please try again later"
+    }
+})
+
 router.post("/register",authController.userRegisterController)
-router.post("/login",authController.userLoginController)
+router.post("/login",limiter,authController.userLoginController)
 router.post("/logout", authController.userLogoutController)
 
 module.exports = router
