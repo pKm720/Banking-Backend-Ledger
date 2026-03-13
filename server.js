e6dd2ce9afff3473b75b456cfd41a09d793fa640
pKm720 <priyanshumahato0210@gmail.com>
2026-03-13 16:41:52 +0530

    Final update

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const connectToDB = require("./src/config/db");
 
 connectToDB();
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT_ENV,()=>{
     console.log("Server is running on port 3000");
 })
